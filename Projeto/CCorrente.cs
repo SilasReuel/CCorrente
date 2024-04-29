3e59c6ac6c1e6bf8b38ab6e244233383e81ba654
ra2357071 <ra2357071@LDI316>
2024-04-29 12:01:30 -0300

    Add project files.

---
+++ Projeto/CCorrente.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Projeto
+{
+    public class CCorrente
+    {
+        public string numero;
+        public double saldo;
+        public double limite;
+        public bool status;
+        public bool especial;
+        public List<Transacao> transacao;
+
+        public CCorrente(string numero, double limite):this()
+        {
+
+            this.numero = numero;
+
+            this.limite = limite;
+        }
+
+        public CCorrente()
+        {
+            this.saldo = 0;
+
+            this.status = true;
+
+            transacao = new List<Transacao> ();
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (saldo - valor > -limite)
+            {
+                saldo -= valor;
+                transacao.Add(new Transacao(valor, 'S'));
+                return true;
+            }
+            return false;
+        }
+
+        public bool Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo += valor;
+                transacao.Add(new Transacao(valor, 'D'));
+                return true;
+            }
+            return false;
+        }
+
+        public bool Transferir(CCorrente destino, double valor)
+        {
+            if (destino.status && Sacar(valor) && destino.Depositar(valor)){
+                transacao[transacao.Count - 1].duplicata = destino.transacao[destino.transacao.Count - 1];
+                destino.transacao[destino.transacao.Count + 1].duplicata = transacao[transacao.Count-1];
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Projeto/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Projeto;
+
+CCorrente conta = new CCorrente("11234", 500);
+List<CCorrente> contas = new List<CCorrente>();
+
+int opc = 0;
+
+do
+{
+    Console.WriteLine("Escolha: 1)Acesso admnistrativo 2)Caixa Eletrônico 0)Sair");
+    opc = Convert.ToInt32(Console.ReadLine());
+    if ( opc == 1)
+    {
+        int opc2 = 0;
+        do
+        {
+            Console.WriteLine("Escolha: 1)Cadastro de Conta Corrente 2)Mostrar Saldos de todas as contas 3)Excluir conta 0)Voltar para Menu Anterior");
+            opc2 = Convert.ToInt32(Console.ReadLine());
+        } while ( opc2 != 0);
+    }else if (opc == 2)
+    {
+        int opc2 = 0;
+        do
+        {
+            Console.WriteLine("Escolha: 1)Ler Conta Corrente 2)Saque 3)Depósito 4)transferência 0)Voltar para Menu Anterior");
+            opc2 = Convert.ToInt32(Console.ReadLine());
+        } while (opc2 != 0);
+    }
+} while (opc!=0);
